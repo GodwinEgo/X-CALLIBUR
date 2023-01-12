51e4a8c287f1c61113c209756b6792df67f0e395
GodwinEgo <godwinehile04@gmail.com>
2023-01-12 16:43:10 +0100

    Almost Done with the navbar

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+/*======================HEADER BACKGROUND===================== */
 var className = 'inverted';
 var scrollTrigger = 60;
 
@@ -11,3 +12,44 @@ window.onscroll = function ()
     document.getElementsByTagName( 'header' )[ 0 ].classList.remove( className );
   }
 }
+
+
+
+
+/*=========================BACK TO TOP==================== */
+var btn = $( '#button' );
+
+$( window ).scroll( function ()
+{
+  if ( $( window ).scrollTop() > 100 )
+  {
+    btn.addClass( 'show' );
+  } else
+  {
+    btn.removeClass( 'show' );
+  }
+} );
+
+btn.on( 'click', function ( e )
+{
+  e.preventDefault();
+  $( 'html, body' ).animate( { scrollTop: 0 }, '100' );
+} );
+
+
+// Back to top button
+$( window ).scroll( function ()
+{
+  if ( $( this ).scrollTop() > 100 )
+  {
+    $( '#button' ).fadeIn( 'slow' );
+  } else
+  {
+    $( '#button' ).fadeOut( 'slow' );
+  }
+} );
+$( '#button' ).click( function ()
+{
+  $( 'html, body' ).animate( { scrollTop: 0 }, 1500, 'easeInOutExpo' );
+  return false;
+} );
